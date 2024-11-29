3232232f663999de8fba84ff6134a3b25662549c
0xvoidmain <hoangtung.utc@gmail.com>
2024-11-29 12:15:30 +0700

    fix image fake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 // import BannerSection from '@/views/home/banner/BannerSection'
 // import HotRewardSection from '@/views/home/hot-reward/HotRewardSection'
 import { getMainLayout } from '@/layouts'
+import Image from 'next/image';
 
 const Home = () => {
   return (
     <div>
       {/* <BannerSection />
       <HotRewardSection /> */}
-      <img src='/fakelove.jpg' width="100%"/>
+      
+      <Image 
+        src="/fakelove.jpg" 
+        alt="Fake Love" 
+        layout="responsive" 
+        width={100}
+        height={100}
+      />
     </div>
   )
 }
