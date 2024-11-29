d5bc278bd3a6068786fe0411f25a51fc9e00398c
0xvoidmain <hoangtung.utc@gmail.com>
2024-11-29 12:02:40 +0700

    add fake home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import BannerSection from '@/views/home/banner/BannerSection'
-import HotRewardSection from '@/views/home/hot-reward/HotRewardSection'
+// import BannerSection from '@/views/home/banner/BannerSection'
+// import HotRewardSection from '@/views/home/hot-reward/HotRewardSection'
 import { getMainLayout } from '@/layouts'
 
 const Home = () => {
   return (
-    <div className="w-screen">
-      <BannerSection />
-      <HotRewardSection />
+    <div>
+      {/* <BannerSection />
+      <HotRewardSection /> */}
+      <img src='/fakelove.jpg' width="100%"/>
     </div>
   )
 }
