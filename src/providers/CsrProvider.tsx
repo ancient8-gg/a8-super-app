5c03ed321420dc69b0d0667b80a9f4bac1b368e0
Danial Dinh <danialdinh@gmail.com>
2024-12-13 15:35:47 +0700

    chore: bump ui pkg version

---
+++ src/providers/CsrProvider.tsx
@@ -0,0 +1,13 @@
+import dynamic from 'next/dynamic'
+import { Fragment, ReactNode } from 'react'
+
+export default dynamic(
+  () =>
+    Promise.resolve(({ children }: { children: ReactNode }) => {
+      return <Fragment>{children}</Fragment>
+    }),
+  {
+    ssr: false,
+    loading: () => <Fragment />,
+  },
+)
