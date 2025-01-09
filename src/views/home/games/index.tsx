de255dbae154ae53ca67e4a133a30296ec7dc1f7
tuankiet99 <tuankiet@ancient8.gg>
2025-01-09 16:23:46 +0700

    fix: style scrollbar, show ellipsis when long text

--- src/views/home/games/index.tsx
+++ src/views/home/games/index.tsx
@@ -26,6 +26,7 @@ function GamesSection() {
               { lg: 24, sm: 12, xs: 12 },
             ]}
             justify="start"
+            className="h-[434px] mobile:h-[auto]"
           >
             {games.map((game, index) => (
               <Col
